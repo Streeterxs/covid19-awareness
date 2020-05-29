eeddd28225cb476d183b6c0923c98fbc95761177
Streeterxs <afonso_mg3@hotmail.com>
2020-05-29 15:42:35 -0300

    Backend - Server: changed port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,6 @@ import { createServer } from 'http';
 
 const server = createServer(app.callback());
 
-server.listen('3232', () => {
+server.listen('3333', () => {
     console.log('server on');
 });
